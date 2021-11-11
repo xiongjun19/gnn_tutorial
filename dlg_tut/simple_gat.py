9e0fd70c2d9cbaacf06861ab9d00d23b704409fd
Ubuntu <ubuntu@ip-172-31-9-92.us-west-2.compute.internal>
2021-11-11 19:16:27 +0800

    adding dlg tutorial and pyg gat model

---
+++ dlg_tut/simple_gat.py
@@ -0,0 +1,87 @@
+# coding=utf8
+
+import dgl
+import dgl.data as dg_data
+# from dgl.nn import GraphConv
+from dgl.nn import GATConv
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GATConv(in_feats, h_feats, 4)
+        self.conv2 = GATConv(h_feats, num_classes, 4)
+
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = h.mean(1)
+        h = F.relu(h)
+        # h = F.dropout(h)
+        h = self.conv2(g, h)
+        h = h.mean(1)
+        return h
+
+
+def main(model_path):
+    ds, g = get_graph()
+    model = GCN(g.ndata['feat'].shape[1], 16, ds.num_classes)
+    train(g, model, model_path)
+
+
+def get_graph():
+    ds = dg_data.CoraGraphDataset()
+    print("Number of categories: ", ds.num_classes)
+    g = ds[0]
+    print("Node features")
+    print(g.ndata)
+    print('Egde features')
+    print(g.edata)
+    return ds, g
+
+
+def train(g, model, model_path):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+    best_val_acc = 0.
+    best_test_acc = 0.
+    epochs = 1000
+    g = g.to(device)
+    in_feat = g.ndata['feat'].to(device)
+    labels = g.ndata['label'].to(device)
+    train_mask = g.ndata['train_mask'].to(device)
+    val_mask = g.ndata['val_mask'].to(device)
+    test_mask = g.ndata['test_mask'].to(device)
+    model = model.to(device)
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    for epoch in tqdm(range(epochs), "iterating training"):
+        logits = model(g, in_feat)
+        preds = logits.argmax(1)
+        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_acc = calc_metric(preds, labels, train_mask)
+        val_acc = calc_metric(preds, labels, val_mask)
+        test_acc = calc_metric(preds, labels, test_mask)
+        if best_val_acc < val_acc:
+            best_val_acc = val_acc
+            best_test_acc = test_acc
+            torch.save(model, model_path)
+        print('In epoch {}, loss: {:.4f}, train acc {:.4f} \
+                val acc {:.4f} test acc {:.4f}'.format(epoch, loss.item(),
+                    train_acc, best_val_acc, best_test_acc))
+
+def calc_metric(preds, labels, mask):
+    acc = (preds[mask] == labels[mask]).float().mean()
+    return acc
+
+
+
+if __name__ == '__main__':
+    main("gat_dgl_cora.pth")
+
---
+++ pyg_tut/gat_ex.py
@@ -0,0 +1,61 @@
+# coding=utf8
+
+# import torch_geometric as tg
+from torch_geometric.datasets import Planetoid
+import torch
+import torch.nn.functional as F
+# from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
+from tqdm import tqdm
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, dataset):
+        super().__init__()
+        self.conv1 = GATConv(dataset.num_node_features, 16)
+        self.conv2 = GATConv(16, dataset.num_classes)
+
+    # def forward(self, data):
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        # x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+
+def main(model_path):
+    dataset = Planetoid(root='/tmp/Cora', name='Cora')
+    print(dataset[0])
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+    model = GCN(dataset).to(device)
+    data = dataset[0].to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
+    train(model, data, optimizer)
+    acc = eval_model(model, data)
+    print(f"accuray is {acc}")
+    torch.save(model, model_path)
+
+
+def train(model, data, optimizer):
+    model.train()
+    epochs = 400
+    for epoch in tqdm(range(epochs), f'training epoch '):
+        optimizer.zero_grad()
+        out = model(data.x, data.edge_index)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+
+
+def eval_model(model, data):
+    model.eval()
+    pred = model(data.x, data.edge_index).argmax(dim=1)
+    correct = (data.y[data.test_mask] == pred[data.test_mask]).sum()
+    acc = int(correct) / int(data.test_mask.sum())
+    return acc
+
+
+if __name__ == '__main__':
+    main('gat_cora.pth')
